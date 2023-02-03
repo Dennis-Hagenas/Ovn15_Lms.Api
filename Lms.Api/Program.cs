360642b6ef2939947b4ac0552d87ed16258fe40e
Dennis Hagenäs <dh_lexicon@outlook.com>
2023-02-03 16:26:07 +0100

    added dbset

--- Lms.Api/Program.cs
+++ Lms.Api/Program.cs
@@ -1,8 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Lms.Data.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<LmsApiContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("LmsApiContext") ?? throw new InvalidOperationException("Connection string 'LmsApiContext' not found.")));
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt => opt.ReturnHttpNotAcceptable = true)
+    .AddNewtonsoftJson()
+    .AddXmlDataContractSerializerFormatters();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
